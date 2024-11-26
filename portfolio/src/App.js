4df9d34310a2d154f673b8186aae105300c1137f
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-11-27 00:29:57 +0530

    setup files

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    
+    <div className='p-[20px]  '>
+      
+        <Navbar/>
+        <Home/>
+    </div>
+
+  );
+}
+
+export default App;
